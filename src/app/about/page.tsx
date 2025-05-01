79ab2dc5745b8bd946d4f69e42b79bfdf13f95a4
NorrakDev <norrak.dev@gmail.com>
2025-05-01 23:26:50 +0700

    about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,10 +14,12 @@ export default function Page() {
 
   return (
     <div>
-      <h1 className="text-[20vw] font-medium -tracking-widest leading-[100%] p-8">
-        about
-      </h1>
-
+      <div className="h-[60vh] flex items-end">
+        <h1 className="text-[20vw] font-medium -tracking-widest leading-[100%] p-8">
+          about
+        </h1>
+      </div>
+      
       <div className="w-full px-8">
         <img
           src="https://miro.medium.com/v2/resize:fit:1100/format:webp/1*yoxeYaIM-2kjnZqUkO6ZNA.gif"
@@ -26,17 +28,122 @@ export default function Page() {
         />
       </div>
 
+      {/* 01 INTRODUCING SECTION */}
       <section className="flex flex-col w-full px-8">
         <div className="about-section-header w-full z-3 flex justify-between sticky top-1/2 text-lg font-medium">
-          <span>01</span>
+          <span>01/03</span>
           <span>Introduction</span>
         </div>
         <div className="about-section w-full mt-[5vw]">
           <div className="relative">
-            <div className="about-large-title relative min-h-[60vw] flex flex-col justify-center items-center">
-              <div className="text-[24vw] font-semibold -tracking-wider leading-none">
+            <div className="min-h-screen flex flex-col justify-center items-center my-[20vh]">
+              <h1 className="text-[30vw] font-medium -tracking-widest leading-none">
                 hello
+              </h1>
+            </div>
+            <div className="min-h-screen flex flex-col justify-center items-center my-[20vh]">
+              <p className="text-[2.5vw] font-normal -tracking-wider leading-tight text-center">
+                i’m San Soeng from Cambodia <br />and i’ve been
+              </p>
+            </div>
+            <div className="min-h-screen flex flex-col justify-center items-center my-[20vh]">
+              <h2 className="text-[12vw] font-normal -tracking-widest leading-[95%] text-center">
+                creating<br />
+                standout digital<br />
+                design since<br />
+                2018
+              </h2>
+            </div>
+            <div className="min-h-screen flex flex-col justify-center items-center my-[20vh]">
+              <h2 className="w-[45vw] text-[2vw] font-normal -tracking-wider leading-tight text-center">
+                with six years of experience in software development 
+                companies and creative studio, I’ve created exceptional 
+                user experiences that truly stand out. My career has 
+                spanned in-house roles, agency projects, and freelance 
+                opportunities, collaborating across diverse industries 
+                such as finance, banking, e-commerce, entertainment, 
+                education, fintech, and more...
+              </h2>
+            </div>
+            <div className="min-h-screen flex flex-col justify-end items-center gap-y-60 py-[20vh]">
+              <div className="flex flex-col items-center gap-y-2">
+                <h3 className="text-[8.5vw] font-normal -tracking-wider leading-[75%]">40<span className="align-top">+</span></h3>
+                <p className="text-5xl -tracking-widest">projects done</p>
               </div>
+
+              <div className="flex flex-col items-center gap-y-2">
+                <h3 className="text-[8.5vw] font-normal -tracking-wider leading-[75%]">5<span className="align-top">+</span></h3>
+                <p className="text-5xl -tracking-widest">year of experience</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* 02 EXPERIENCE SECTION */} 
+      <section className="flex flex-col w-full px-8 bg-black text-white">
+        <div className="about-section-header w-full z-3 flex justify-between sticky top-1/2 text-lg font-medium">
+          <span>02/03</span>
+          <span>experience</span>
+        </div>
+        <div className="about-section w-full mt-[5vw]">
+          <div className="relative">
+            <div className="min-h-screen flex flex-col justify-center items-center my-[20vh]">
+              <h1 className="text-[30vw] font-medium -tracking-widest leading-none">
+                also
+              </h1>
+            </div>
+            <div className="min-h-screen flex flex-col justify-center items-center my-[20vh]">
+              <p className="text-[2.5vw] font-normal -tracking-wider leading-tight text-center">
+                i’m a designer<br />that crafting
+              </p>
+            </div>
+            <div className="min-h-screen flex flex-col justify-center items-center my-[20vh]">
+              <h2 className="text-[12vw] font-normal -tracking-widest leading-[95%] text-center">
+              compelling<br />
+              visual narratives<br />
+              that captivate 
+              </h2>
+            </div>
+            <div className="min-h-screen flex flex-col justify-center items-center my-[20vh]">
+              <h2 className="w-[45vw] text-[2vw] font-normal -tracking-wider leading-tight text-center">
+              by combining keen attention to detail with strong 
+              collaborative skills. With experience leading design 
+              teams, I excel at solving complex design challenges and 
+              transforming them into clear, engaging, and impactful
+              visual solutions.
+              </h2>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* 03 SERVICE/APPROACH SECTION */}
+      <section className="flex flex-col w-full px-8">
+        <div className="about-section-header w-full z-3 flex justify-between sticky top-1/2 text-lg font-medium">
+          <span>03/03</span>
+          <span>service/approach</span>
+        </div>
+        <div className="about-section w-full mt-[5vw]">
+          <div className="relative">
+            <div className="min-h-screen flex flex-col justify-center items-center my-[20vh]">
+              <h1 className="text-[30vw] font-medium -tracking-widest leading-none">
+                finally
+              </h1>
+            </div>
+            <div className="min-h-screen flex flex-col justify-center items-center my-[20vh]">
+              <p className="text-[2.5vw] font-normal -tracking-wider leading-tight text-center">
+              I'm varied in my approach<br />of creating
+              </p>
+            </div>
+            <div className="min-h-screen flex flex-col justify-center items-center my-[20vh]">
+              <h2 className="w-[45vw] text-[2vw] font-normal -tracking-wider leading-tight text-center">
+              I conduct thorough research to understand project goals, 
+              then move to design exploration, developing concepts 
+              and user goals, and finally, create engaging, interactive
+              products that combine aesthetics user interface and
+              seamlessly user experience.
+              </h2>
             </div>
           </div>
         </div>
--- src/components/AboutMeSection.tsx
+++ src/components/AboutMeSection.tsx
@@ -89,19 +89,15 @@ export default function AboutMeSection() {
               <div className="flex flex-col gap-y-6">
                 <div className="flex gap-x-1 items-end">
                   <h2 className="text-[8.5vw] font-medium leading-[75%]">5</h2>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 45.402 45.402" className="w-8 h-8">
-                    <path d="M41.267,18.557H26.832V4.134C26.832,1.851,24.99,0,22.707,0c-2.283,0-4.124,1.851-4.124,4.135v14.432H4.141   c-2.283,0-4.139,1.851-4.138,4.135c-0.001,1.141,0.46,2.187,1.207,2.934c0.748,0.749,1.78,1.222,2.92,1.222h14.453V41.27   c0,1.142,0.453,2.176,1.201,2.922c0.748,0.748,1.777,1.211,2.919,1.211c2.282,0,4.129-1.851,4.129-4.133V26.857h14.435   c2.283,0,4.134-1.867,4.133-4.15C45.399,20.425,43.548,18.557,41.267,18.557z" />
-                  </svg>
+                  <span className='text-[4.5vw] leading-[75%]'>+</span>
                 </div>
                 <p className="text-3xl font-medium -tracking-wider">years experience</p>
               </div>
 
               <div className="flex flex-col gap-y-6">
                 <div className="flex gap-x-1 items-end">
-                  <h2 className="text-[8.5vw] font-medium leading-[75%]">150</h2>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 45.402 45.402" className="w-8 h-8">
-                    <path d="M41.267,18.557H26.832V4.134C26.832,1.851,24.99,0,22.707,0c-2.283,0-4.124,1.851-4.124,4.135v14.432H4.141   c-2.283,0-4.139,1.851-4.138,4.135c-0.001,1.141,0.46,2.187,1.207,2.934c0.748,0.749,1.78,1.222,2.92,1.222h14.453V41.27   c0,1.142,0.453,2.176,1.201,2.922c0.748,0.748,1.777,1.211,2.919,1.211c2.282,0,4.129-1.851,4.129-4.133V26.857h14.435   c2.283,0,4.134-1.867,4.133-4.15C45.399,20.425,43.548,18.557,41.267,18.557z" />
-                  </svg>
+                  <h2 className="text-[8.5vw] font-medium leading-[75%]">40</h2>
+                  <span className='text-[4.5vw] leading-[75%]'>+</span>
                 </div>
                 <p className="text-3xl font-medium -tracking-wider">projects done</p>
               </div>
